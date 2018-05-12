5b5e4d0ca8d8195fcda5b81fb802c21d52f394bb
bario <bario@DESKTOP-HVQ0S6U>
2018-05-12 19:45:28 +0300

    update

--- src/logic/Semaphore.java
+++ src/logic/Semaphore.java
@@ -1,11 +1,22 @@
 package logic;
 
+import java.awt.Color;
 import java.util.Timer;
 
 public class Semaphore {
 	
-	private Timer timer;
-	private Port port;
+	public Color color;
+	public int cordX, cordY;
+	
+	public Semaphore(Color color, int cordX, int cordY) {
+		super();
+		this.color = color;
+		this.cordX = cordX;
+		this.cordY = cordY;
+	}
+	
+	
+	
 	
 	
 }
